88367c044c7dbefdfad9cca6b727712a3c92b0e9
ArvisP <arvis.peralta@gmail.com>
2017-10-30 19:40:06 -0400

    Finished routes.py

--- dateapp/routes.py
+++ dateapp/routes.py
@@ -2,3 +2,21 @@ from . import app
 import datetime
 from flask import request
 from flask import render_template
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/calculated")
+def calc():
+    if request.args:
+        date = request.args.get("date")
+        year, month, day = (int(x) for x in date.split('-'))
+        ans = datetime.date(year, month, day)
+        month_lst = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July',
+              'August', 'September', 'October', 'November', 'December']
+        answer = ans.strftime("%A") +", "+month_lst[month-1]+" "+str(day)+", "+str(year)
+
+        return render_template("index.html", result=answer)
+    else:
+        return render_template("index.html")
